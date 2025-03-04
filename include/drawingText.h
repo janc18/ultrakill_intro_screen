5bfeea5bb8abe79f5fb33e554f938ce8ab93281f
Jose Agustin <josecla011@gmail.com>
2025-03-03 21:47:21 -0600

    added: drawingText.h library

---
+++ include/drawingText.h
@@ -0,0 +1,9 @@
+#ifndef DRAWING_TEXT_H
+#define DRAWING_TEXT_H
+
+int getRemainingChars(const char* text, int currentFPS, int currentFrameCounter, int* currentStringIndex, int* lastIndex);
+
+
+#endif 
+
+
