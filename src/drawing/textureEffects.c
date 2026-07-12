bc5e9cac348d93979a6c465feb39bfc8c156ec27
Jose Agustin <josecla011@gmail.com>
2026-07-12 13:39:13 -0600

    chore: remove comments

--- src/drawing/textureEffects.c
+++ src/drawing/textureEffects.c
@@ -13,21 +13,7 @@ void UpdateFadeOut(float* alpha, bool* fadingOut)
         }
     }
 }
-/*
-int fadeOutTextures(stringData_t* stringData, float alpha, bool fadingOut)
-{
-    for (int i = 0; i < stringData->numberOfStrings; i++)
-    {
-        Color     fadeColor = {255, 255, 255, (unsigned char)(alpha * 255)};
-        Rectangle sourceRec = {0, 0, (float)stringData->StringTextures[i].texture.width, -(float)stringData->StringTextures[i].texture.height};
-        Rectangle destRec   = {stringData->texturePosition[i].x, stringData->texturePosition[i].y, stringData->StringTextures[i].texture.width,
-                               stringData->StringTextures[i].texture.height};
-        Vector2   origin    = {0, 0};
-        DrawTexturePro(stringData->StringTextures[i].texture, sourceRec, destRec, origin, 0.0f, fadeColor);
-    }
-    return 0;
-}
-*/
+
 int fadeOutTexture(Texture2D texture, float alpha, Rectangle sourceRect, Vector2 position)
 {
     Color fadeColor = {255, 255, 255, (unsigned char)(alpha * 255)};
