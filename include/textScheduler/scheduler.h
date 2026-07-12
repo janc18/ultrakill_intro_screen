dcb35b0cbc24cb14f78d0cf7ea7292022471b450
Jose Agustin <josecla011@gmail.com>
2026-07-11 19:41:53 -0600

    chore: wrapped functions

--- include/textScheduler/scheduler.h
+++ include/textScheduler/scheduler.h
@@ -5,6 +5,8 @@ void scheduleSequentially(terminalMessages_t* dispatcher, phrase_t* Phrase);
 void scheduleAllAtTheTime(terminalMessages_t* dispatcher, phrase_t* Phrase);
 void scheduleFadeOutAllSkip(terminalMessages_t* dispatcher);
 void DrawPhrases(terminalMessages_t* d, phrase_t* Phrase);
+void DrawTextV1Terminal(phrase_t *Phrase,int indexDisplacement);
+void DispatchTerminalMessageV1(terminalMessages_t* dispatcher,phrase_t* Phrase, int index);
 
 
 #endif // TEXT_SCHEDULER
